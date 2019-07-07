b0b915512ec390e51a4049a80c3289dc8f651774
cedric e goddard <8ceg6@comcast.net>
2019-07-07 16:29:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,5 +17,4 @@ function logShout(string){
 function sayHiToGrandma(string){
   if (string===string.toLowerCase()) return "i can't hear you"
   else if (string===string.toLowerCase()) return "YES INDEED"
- else if (string==="i love you, Grandma.") return "I love you, too"
 }
